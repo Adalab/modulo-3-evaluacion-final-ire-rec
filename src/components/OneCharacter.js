6efc7e7882da1580572e210ca54b3a50ec1c4c80
ire-rec <i.recavarren@gmail.com>
2024-04-15 11:23:48 +0200

    fix character list

--- src/components/OneCharacter.js
+++ src/components/OneCharacter.js
@@ -1,19 +1,20 @@
-import '../styles/App.scss';
-import { Link } from 'react-router-dom';
-
+import "../styles/App.scss";
+import { Link } from "react-router-dom";
 
 function OneCharacter(props) {
-
-    return (
-
-        <li className="li_charactersList"
-        >
-            <Link to={`/user/${props.charactersList.id}`} className="link" >
-                <img src={props.charactersList.image} className="image_charactersList" alt="image_charactersList"></img>
-                <p className="name_charactersList">{`Nombre: ${props.charactersList.name}`}</p><p className="species_charactersList">{`Especie: ${props.charactersList.species}`}</p>
-            </Link> </li>
-    )
+  return (
+    <li className='li_charactersList'>
+      <Link to={`/user/${props.charactersList.id}`} className='link'>
+        <img
+          src={props.charactersList.image}
+          className='image_charactersList'
+          alt='image_charactersList'
+        ></img>
+        <p className='name_charactersList'>{`${props.charactersList.name}`}</p>
+        <p className='species_charactersList'>{`${props.charactersList.species}`}</p>
+      </Link>{" "}
+    </li>
+  );
 }
-
 
 export default OneCharacter;
